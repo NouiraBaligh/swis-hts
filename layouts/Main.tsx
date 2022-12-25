52d1f92a9aea71e4b8b1bc2b8510239572a468f6
Nouira Baligh <nouirabaligh009@gmail.com>
2022-12-25 01:16:57 +0100

    fix

--- layouts/Main.tsx
+++ layouts/Main.tsx
@@ -7,7 +7,7 @@ type LayoutType = {
   children?: React.ReactNode;
 }
 
-export default ({ children, title = 'Next.js Ecommerce' }: LayoutType) => {
+export default ({ children, title = 'Swiss High Texh Services' }: LayoutType) => {
   const router = useRouter();
   const pathname = router.pathname;
 
