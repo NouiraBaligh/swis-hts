969f42e58f527d4e2e93a702926b5f5b34b88377
Nouira Baligh <nouirabaligh009@gmail.com>
2023-02-11 22:55:43 +0100

    rr

--- utils/data/products.ts
+++ utils/data/products.ts
@@ -61,7 +61,7 @@ export const products = [
   {
     id: '2',
     name: 'development of a mobile application (hybrid) with flutter',
-    price: 89.99,
+    price: 555.99,
     quantityAvailable: 2,
 
     currentPrice: 555,
@@ -119,7 +119,7 @@ export const products = [
   {
     id: '3',
     name: 'development of customs sections and pages in a Shopify theme',
-    price: 89.99,
+    price: 355,
     quantityAvailable: 2,
    
     currentPrice: 355,
@@ -177,10 +177,10 @@ export const products = [
   {
     id: '4',
     name: 'create a multi products Shopify store',
-    price: 89.99,
+    price: 655,
     quantityAvailable: 2,
     
-    currentPrice: 115,
+    currentPrice: 655,
     
     images: [
       '/images/products/product-4.jpg',
@@ -235,10 +235,10 @@ export const products = [
   {
     id: '5',
     name: 'Analyze data in Excel, google Sheets or Accesssss',
-    price: 89.99,
+    price: 755.00,
    
     category: 'T-shirt',
-    currentPrice: 155.00,
+    currentPrice: 755.00,
    
     images: [
       '/images/products/product-5.jpg',
@@ -293,7 +293,7 @@ export const products = [
   {
     id: '6',
     name: 'Create an Android & IOS ecommerce mobile application',
-    price: 89.99,
+    price: 455,
    currentPrice: 455,
    images: [
       '/images/products/product-6.jpg',
@@ -344,115 +344,7 @@ export const products = [
         punctuation: 5
       },
     ]
-  }, {
-    id: '7',
-    name: 'Create a Business plan for Startups',
-    price: 89.99,
-   currentPrice: 755,
-   images: [
-      '/images/products/4334.webp',
-    ],
-    punctuation: {
-      countOpinions: 81,
-      punctuation: 4.5,
-      votes: [
-        {
-          value: 1,
-          count: 1
-        },
-        {
-          value: 2,
-          count: 10
-        },
-        {
-          value: 3,
-          count: 10
-        },
-        {
-          value: 4,
-          count: 20
-        },
-        {
-          value: 5,
-          count: 40
-        },
-      ]
-    },
-    reviews: [
-      {
-        name: 'William ',
-        avatar: '/images/12211.jpeg',
-        description: '<p>Theme was created to demand the smallest possible amount of efforts from the user. To install the theme or skins you only have to click one button. Elementor Page Builder is there for design customization.</p>',
-        punctuation: 2
-      },
-      {
-        name: '   Madison',
-        avatar: '/images/443.png',
-        description: '<p>Besides that, they also add some newly designed skins and ready-made pages, expanding the capabilities available for users.</p>',
-        punctuation: 4
-      },
-      {
-        name: '     Isabella ',
-        avatar: '/images/213.jpeg',
-        description: '<p>A new update comes every month, so using theme you could be sure that it is the best possible solution.</p>',
-        punctuation: 5
-      },
-    ]
-  }, {
-    id: '8',
-    name: 'A course for marketing digital ',
-    price: 89.99,
-   currentPrice: 655,
-   images: [
-      '/images/products/332232.webp',
-    ],
-    punctuation: {
-      countOpinions: 82,
-      punctuation: 4.5,
-      votes: [
-        {
-          value: 1,
-          count: 1
-        },
-        {
-          value: 2,
-          count: 10
-        },
-        {
-          value: 3,
-          count: 10
-        },
-        {
-          value: 4,
-          count: 20
-        },
-        {
-          value: 5,
-          count: 40
-        },
-      ]
-    },
-    reviews: [
-      {
-        name: 'William ',
-        avatar: '/images/12211.jpeg',
-        description: '<p>Theme was created to demand the smallest possible amount of efforts from the user. To install the theme or skins you only have to click one button. Elementor Page Builder is there for design customization.</p>',
-        punctuation: 2
-      },
-      {
-        name: '   Madison',
-        avatar: '/images/443.png',
-        description: '<p>Besides that, they also add some newly designed skins and ready-made pages, expanding the capabilities available for users.</p>',
-        punctuation: 4
-      },
-      {
-        name: '     Isabella ',
-        avatar: '/images/213.jpeg',
-        description: '<p>A new update comes every month, so using theme you could be sure that it is the best possible solution.</p>',
-        punctuation: 5
-      },
-    ]
-  },
+  }, 
 ];
 
 export default products;
