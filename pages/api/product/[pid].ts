32e57a5d0ce055d38274ad29af846dede9d4e290
Nouira Baligh <nouirabaligh009@gmail.com>
2023-02-15 22:01:32 +0100

    rfrf

--- pages/api/product/[pid].ts
+++ pages/api/product/[pid].ts
@@ -9,6 +9,6 @@ export default (req: NextApiRequest, res: NextApiResponse) => {
   } = req
 
   const product = products.find(x => x.id === pid);
-    res.setHeader('Cache-Control', 'public, max-age=0'); 
+  res.setHeader('Cache-Control', 'no-cache, must-revalidate, max-age=0');
   res.status(200).json(product);
 }
