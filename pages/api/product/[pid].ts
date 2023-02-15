ae557c1fe27d39f00f5cfe945fb16f93174e4f9b
Nouira Baligh <nouirabaligh009@gmail.com>
2023-02-15 21:55:30 +0100

    deded

--- pages/api/product/[pid].ts
+++ pages/api/product/[pid].ts
@@ -1,14 +1,25 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-// fake data
 import products from '../../../utils/data/products';
+import { NextApiRequest, NextApiResponse } from 'next';
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
   const {
     query: { pid },
-  } = req
+  } = req;
 
-  const product = products.find(x => x.id === pid);
+  switch (req.method) {
+    case 'GET':
+      const product = products.find(x => x.id === pid);
+      if (product) {
   res.setHeader('Cache-Control', 'public, max-age=0'); 
-  res.status(200).json(product);
-}
+        res.status(200).json(product);
+      } else {
+        res.status(404).json({ message: `Product with ID ${pid} not found` });
+      }
+      break;
+    case 'POST':
+      // Handle POST request
+      break;
+    default:
+      res.status(405).json({ message: 'Method not allowed' });
+  }
+};
