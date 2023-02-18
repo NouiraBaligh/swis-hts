74ad3f8f7f3fa063cdbf5384752ebe99853fd2f6
Nouira Baligh <nouirabaligh009@gmail.com>
2023-02-18 14:51:10 +0100

    dedc

--- utils/data/products.ts
+++ utils/data/products.ts
@@ -344,13 +344,14 @@ export const products = [
         punctuation: 5
       },
     ]
-  },   {
-    id: '6',
-    name: 'Create an Android  ecommerce mobile application for shopify',
+  }, 
+  {
+    id: '7',
+    name: 'Create a devops session application ',
     price: 955,
    currentPrice: 955,
    images: [
-      '/images/products/product-3.jpg',
+      '/images/products/product-7.jpg',
     ],
     punctuation: {
       countOpinions: 81,
@@ -398,7 +399,62 @@ export const products = [
         punctuation: 5
       },
     ]
-  }, 
+  },
+  {
+    id: '8',
+    name: 'Create a scrum session for startups ',
+    price: 1100,
+   currentPrice: 1100,
+   images: [
+      '/images/products/product-8.jpg',
+    ],
+    punctuation: {
+      countOpinions: 81,
+      punctuation: 4.5,
+      votes: [
+        {
+          value: 1,
+          count: 1
+        },
+        {
+          value: 2,
+          count: 10
+        },
+        {
+          value: 3,
+          count: 10
+        },
+        {
+          value: 4,
+          count: 20
+        },
+        {
+          value: 5,
+          count: 40
+        },
+      ]
+    },
+    reviews: [
+      {
+        name: 'William ',
+        avatar: '/images/12211.jpeg',
+        description: '<p>Theme was created to demand the smallest possible amount of efforts from the user. To install the theme or skins you only have to click one button. Elementor Page Builder is there for design customization.</p>',
+        punctuation: 2
+      },
+      {
+        name: '   Madison',
+        avatar: '/images/443.png',
+        description: '<p>Besides that, they also add some newly designed skins and ready-made pages, expanding the capabilities available for users.</p>',
+        punctuation: 4
+      },
+      {
+        name: '     Isabella ',
+        avatar: '/images/213.jpeg',
+        description: '<p>A new update comes every month, so using theme you could be sure that it is the best possible solution.</p>',
+        punctuation: 5
+      },
+    ]
+  },
 ];
 
 export default products;
