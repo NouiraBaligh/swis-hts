bf8c46bfe5408480495fb57eb82f7d317f05fa30
Nouira Baligh <nouirabaligh009@gmail.com>
2023-02-18 14:43:28 +0100

    ttttt

--- pages/api/product/[pid].ts
+++ pages/api/product/[pid].ts
@@ -1,14 +1,26 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-// fake data
+import { NextApiRequest, NextApiResponse } from 'next';
 import products from '../../../utils/data/products';
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
   const {
-    query: { pid },
-  } = req
+    query: { pid, timestamp },
+  } = req;
 
-  const product = products.find(x => x.id === pid);
-  res.setHeader('Cache-Control', 'no-cache, must-revalidate, max-age=0');
+  const product = products.find((x) => x.id === pid);
+
+  if (!product) {
+    res.status(404).json({ message: 'Product not found' });
+    return;
+  }
+
+  // If a timestamp is provided, set the Cache-Control header to 0 seconds
+  // to ensure that the browser always fetches the latest version of the data
+  if (timestamp) {
+    res.setHeader('Cache-Control', 'public, max-age=0, must-revalidate');
+  } else {
+    // Otherwise, set a short cache time to improve performance
+    res.setHeader('Cache-Control', 'public, max-age=60, stale-while-revalidate=30');
+  }
+
   res.status(200).json(product);
-}
+};
